33fa020872d27dd68fd40f1b0afa68575b8ac673
rogerbf <roger@bergfalk.nu>
2017-02-17 18:13:54 +0100

    100% coverage

--- watch-pid.test.js
+++ watch-pid.test.js
@@ -1,5 +1,18 @@
 const watch = require(`./watch-pid`)
 
-test(``, () => {
-  expect(watch).toBeDefined()
+test(`watch`, () => {
+  const kill = jest.fn(() => {
+    kill.mock.calls.length > 1 &&
+    (() => { throw Error(`done`) })()
+  })
+  const setTimeout = jest.fn(fn => fn())
+  const pid = 42
+  const callback = jest.fn()
+  watch(kill, setTimeout, pid, callback)
+
+  expect(kill).toHaveBeenCalledWith(pid, 0)
+  expect(kill).toHaveBeenCalledTimes(2)
+  expect(setTimeout).toHaveBeenCalledTimes(2)
+  expect(setTimeout.mock.calls[0]).toContain(1500)
+  expect(callback).toHaveBeenCalledWith(pid)
 })
